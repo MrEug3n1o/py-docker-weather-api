67f8cb256283ba77e57c15905663a3e8481ddab8
mr_eugene <roberttimethone@gmail.com>
2025-12-01 22:06:05 +0100

    sol

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
+import requests
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    weather = {
+        "q": "Paris",
+        "key": os.getenv("API")
+    }
+    url = "http://api.weatherapi.com/v1/current.json"
+    response = requests.get(url, params=weather)
+    print(response.json())
 
 
 if __name__ == "__main__":
