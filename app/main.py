0a454e5fbce7de44b94fae7d25eacf38c5568cde
mr_eugene <roberttimethone@gmail.com>
2025-12-01 22:18:45 +0100

    sol

--- app/main.py
+++ app/main.py
@@ -2,13 +2,15 @@ import requests
 import os
 
 
+WEATHER_API_URL = "http://api.weatherapi.com/v1/current.json"  # noqa:  N806
+CITY = "Paris"  # noqa:  N806
+
+
 def get_weather() -> None:
-    CITY = "Paris"
     weather = {
         "q": CITY,
         "key": os.getenv("API")
     }
-    WEATHER_API_URL = "http://api.weatherapi.com/v1/current.json"
     response = requests.get(WEATHER_API_URL, params=weather)
     print(response.json())
 
